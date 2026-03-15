c337ba0600e176882e39edc5d37f00235f3654b4
gkwitch <gleicykethelin@ufrrj.br>
2026-03-15 19:29:15 -0300

    adicionando ex do livro

---
+++ 3_periodo/Java/pratice/cap_2/src/ex_2_3.java
@@ -0,0 +1,37 @@
+public class ex_2_3 {
+}
+    class Ex_a {
+        int c, thisIsAVariable, q76354, number;
+}
+    //b
+   class Ex_b {
+        void main() {
+            IO.println("Write a number: ");
+        }
+    }
+    class Ex_c {
+        int value;
+        void main(){
+            var tx_num = IO.readln("Write a number: ");
+            int value = Integer.parseInt(tx_num);
+        }
+    }
+    class Ex_d {
+        void main() {
+            IO.println("This is a Java Program");
+        }
+    }
+    class Ex_e{
+        void main() {
+            System.out.printf("%s%n%s%n", "This is a Java", "program");
+        }
+
+    }
+    class Ex_f{
+        void main() {
+            var tx_num = IO.readln("Write a number: ");
+            int number = Integer.parseInt(tx_num);
+            if (number != 7)
+                IO.println("The variable number is not equal to 7");
+        }
+    }
